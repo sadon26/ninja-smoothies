68df118841cb4406609202f68d41f1b67844c318
sadon26 <ashadayosamuel@gmail.com>
2020-07-15 10:02:42 +0100

    update made

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -16,11 +16,11 @@
               </li>
             </ul>
           </div>
-          <span class="btn-floating btn-large halfway-fab pink">
-            <router-link :to="{ name: 'EditSmoothie', params: { smoothie_slug: smoothie.slug } }">
+          <router-link :to="{ name: 'EditSmoothie', params: { smoothie_slug: smoothie.slug } }">
+            <span class="btn-floating btn-large halfway-fab pink">
               <i class="material-icons edit">edit</i>
-            </router-link>
-          </span>
+            </span>
+          </router-link>
         </div>
     </div>
 </template>
